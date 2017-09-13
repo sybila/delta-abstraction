bad2c1f03f20452031a3d2584448868de080c254
Samuel Pastva <daemontus@gmail.com>
2017-09-13 18:48:19 +0200

    fun fun fun

--- src/main/java/dreal/partition.kt
+++ src/main/java/dreal/partition.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.newFixedThreadPoolContext
 import java.io.File
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.ArrayList
 
 val pool = newFixedThreadPoolContext(4, "foo")
 
-suspend fun ModelFactory.makePartitions(precision: Double = 0.001, timeBound: Double = 6.0) {
+suspend fun ModelFactory.makePartitions(precision: Double = 0.001, timeBound: Double = 1.0) {
     val volumeThreshold = 0.01
     val solver = Grid1Solver(dimensionBounds(2))
 
@@ -19,14 +21,121 @@ suspend fun ModelFactory.makePartitions(precision: Double = 0.001, timeBound: Do
     val unsafe = noFlow
     //val unsafe = splitByParam(noFlow, solver, volumeThreshold, precision, timeBound)
 
+    val c = AtomicInteger(0)
+    val probablyUnsafe = ArrayList<Rect>()
+    println("Unsafe: ${unsafe.size}")
+    unsafe.map { r -> async(pool) {
+        c.incrementAndGet().takeIf { it % 100 == 0 }?.let { println(it) }
+        val admissibleXUp = makeQuery(
+                """
+                        ${makeHead()}
+                        ${r.boundsQuery(x, y)}
+                        (assert (= x ${r.x2}))
+                        (assert (>= 0 ${makeModelEquation(0)}))
+                    """
+        )
+        val slowFlowXUp = makeQuery(
+                """
+                        ${makeHead()}
+                        ${makeHead("_0_0")}
+                        ${makeHead("_0_t")}
+                        (declare-fun time () Real [0.0, $timeBound])
+                        ${makeFlow()}
+                        (assert (= time $timeBound))
+                        (assert (= [x_0_t y_0_t p_0_t] (integral 0. time [x_0_0 y_0_0 p_0_0] flow_1)))
+                        ${r.boundsQuery("x_0_0", "y_0_0")}
+                        ${r.boundsQuery("x_0_t", "y_0_t")}
+                        (assert (= x_0_0 ${r.x2}))
+                        (assert (forall_t 1 [0 time] (and (< x_0_t ${r.x2}) (> x_0_t ${r.x1}) (< y_0_t ${r.y2}) (> y_0_t ${r.y1}))))
+                    """
+        )
+        val admissibleXDown = makeQuery(
+                """
+                        ${makeHead()}
+                        ${r.boundsQuery(x, y)}
+                        (assert (= x ${r.x1}))
+                        (assert (<= 0 ${makeModelEquation(0)}))
+                    """
+        )
+        val slowFlowXDown = makeQuery(
+                """
+                        ${makeHead()}
+                        ${makeHead("_0_0")}
+                        ${makeHead("_0_t")}
+                        (declare-fun time () Real [0.0, $timeBound])
+                        ${makeFlow()}
+                        (assert (= time $timeBound))
+                        (assert (= [x_0_t y_0_t p_0_t] (integral 0. time [x_0_0 y_0_0 p_0_0] flow_1)))
+                        ${r.boundsQuery("x_0_0", "y_0_0")}
+                        ${r.boundsQuery("x_0_t", "y_0_t")}
+                        (assert (= x_0_0 ${r.x1}))
+                        (assert (forall_t 1 [0 time] (and (< x_0_t ${r.x2}) (> x_0_t ${r.x1}) (< y_0_t ${r.y2}) (> y_0_t ${r.y1}))))
+                    """
+        )
+        val admissibleYUp = makeQuery(
+                """
+                        ${makeHead()}
+                        ${r.boundsQuery(x, y)}
+                        (assert (= y ${r.y2}))
+                        (assert (>= 0 ${makeModelEquation(1)}))
+                    """
+        )
+        val slowFlowYUp = makeQuery(
+                """
+                        ${makeHead()}
+                        ${makeHead("_0_0")}
+                        ${makeHead("_0_t")}
+                        (declare-fun time () Real [0.0, $timeBound])
+                        ${makeFlow()}
+                        (assert (= time $timeBound))
+                        (assert (= [x_0_t y_0_t p_0_t] (integral 0. time [x_0_0 y_0_0 p_0_0] flow_1)))
+                        ${r.boundsQuery("x_0_0", "y_0_0")}
+                        ${r.boundsQuery("x_0_t", "y_0_t")}
+                        (assert (= y_0_0 ${r.y2}))
+                        (assert (forall_t 1 [0 time] (and (< x_0_t ${r.x2}) (> x_0_t ${r.x1}) (< y_0_t ${r.y2}) (> y_0_t ${r.y1}))))
+                    """
+        )
+        val admissibleYDown = makeQuery(
+                """
+                        ${makeHead()}
+                        ${r.boundsQuery(x, y)}
+                        (assert (= y ${r.y1}))
+                        (assert (<= 0 ${makeModelEquation(1)}))
+                    """
+        )
+        val slowFlowYDown = makeQuery(
+                """
+                        ${makeHead()}
+                        ${makeHead("_0_0")}
+                        ${makeHead("_0_t")}
+                        (declare-fun time () Real [0.0, $timeBound])
+                        ${makeFlow()}
+                        (assert (= time $timeBound))
+                        (assert (= [x_0_t y_0_t p_0_t] (integral 0. time [x_0_0 y_0_0 p_0_0] flow_1)))
+                        ${r.boundsQuery("x_0_0", "y_0_0")}
+                        ${r.boundsQuery("x_0_t", "y_0_t")}
+                        (assert (= y_0_0 ${r.y1}))
+                        (assert (forall_t 1 [0 time] (and (< x_0_t ${r.x2}) (> x_0_t ${r.x1}) (< y_0_t ${r.y2}) (> y_0_t ${r.y1}))))
+                    """
+        )
+        val safeXUp = !checkNotSat(admissibleXUp) && checkNotSat(slowFlowXUp)
+        val safeXDown = !checkNotSat(admissibleXDown) && checkNotSat(slowFlowXDown)
+        val safeYUp = !checkNotSat(admissibleYUp) && checkNotSat(slowFlowYUp)
+        val safeYDown = !checkNotSat(admissibleYDown) && checkNotSat(slowFlowYDown)
+        val someSafe = safeXUp || safeXDown || safeYUp || safeYDown
+        //val someSafe = checkNotSat(slowFlowXUp, precision) || checkNotSat(slowFlowXDown, precision) || checkNotSat(slowFlowYDown, precision) || checkNotSat(slowFlowYUp, precision)
+        if (someSafe) synchronized(probablyUnsafe) { probablyUnsafe.add(r) }
+    } }.forEach { it.await() }
+
     val output = File("/Users/daemontus/Downloads/test.svg")
     val multiplier = 100.0
+    val shift = 0.0//400.0
     val image = SVG(1000.0, 1000.0) {
         safe.forEach {
-            addRectangle(it.x1 * multiplier, it.y1 * multiplier, it.x2 * multiplier, it.y2 * multiplier, fill = 0.0)
+            addRectangle(it.x1 * multiplier + shift, it.y1 * multiplier + shift, it.x2 * multiplier + shift, it.y2 * multiplier + shift, fill = 0.0)
         }
-        unsafe.forEach {
-            addRectangle(it.x1 * multiplier, it.y1 * multiplier, it.x2 * multiplier, it.y2 * multiplier, fill = 1.0)
+        probablyUnsafe.forEach {
+            addRectangle(it.x1 * multiplier + shift, it.y1 * multiplier + shift, it.x2 * multiplier + shift, it.y2 * multiplier + shift, fill = 1.0)
         }
         /*unsafe.forEach { (it, p) ->
             addRectangle(it.x1 * multiplier, it.y1 * multiplier, it.x2 * multiplier, it.y2 * multiplier, fill = (p?.cardinality ?: 0.0) / solver.TT.cardinality)
