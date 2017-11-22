6cd4b092981bf1a004c8eb06df7cd9554ecbfd10
Samuel Pastva <daemontus@gmail.com>
2017-11-22 11:54:26 +0100

    disable unnecessary svg tasks

--- src/main/java/Experiments.kt
+++ src/main/java/Experiments.kt
@@ -7,21 +7,21 @@ fun main(args: Array<String>) {
 
     PWMA.Approximation
     PWMA.Partition
-    PWMA.Partition.Svg
+    //PWMA.Partition.Svg
     //PWMA.Transitions
     //PWMA.Transitions.Svg
     //PWMA.TerminalComponents
     //PWMA.TerminalComponents.Svg
     Delta.Rectangular.All
-    Delta.Rectangular.All.Svg
+    //Delta.Rectangular.All.Svg
     Delta.Rectangular.States
-    Delta.Rectangular.States.Svg
+    //Delta.Rectangular.States.Svg
     Delta.Rectangular.Transitions
     Delta.Rectangular.Transitions.Svg
     Delta.Rectangular.TerminalComponents
     Delta.Rectangular.TerminalComponents.Svg
     Delta.Rectangular.InitialComponents
-    Delta.Rectangular.InitialComponents.Svg
+    //Delta.Rectangular.InitialComponents.Svg
     //Delta.Rectangular.Cycles
     //Delta.Rectangular.Cycles.Svg
 
